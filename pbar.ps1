81148a195ed60f39d52bf3c6a742a2ef30465706
Michael Treml <miketreml@mac.com>
2022-05-18 15:14:50 -0500

    Update pbar.ps1

--- pbar.ps1
+++ pbar.ps1
@@ -1,5 +1,8 @@
-for ($i = 1; $i -le 100; $i++ )
-{
-Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i
-Start-Sleep -Milliseconds 250
+function test_psf {
+  for ($i = 1; $i -le 100; $i++ )
+  {
+    Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i
+    Start-Sleep -Milliseconds 250
+  }
 }
+test_psf
