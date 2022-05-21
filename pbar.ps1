30eba112ad9475aa0b76786175f63cec15849edf
Michael Treml <miketreml@mac.com>
2022-05-21 15:25:47 -0500

    Update pbar.ps1

--- pbar.ps1
+++ pbar.ps1
@@ -168,8 +168,8 @@ function Show-bar_psf {
 	#$oldFile = Get-Item 'G:\MSSQL_DATA\AxDB*Primary.mdf'
 	
 	$oldFile = Get-Item G:\MSSQL_DATA\AxDB_20220517_Primary.mdf -Exclude AxDB_$dt_Primary.mdf
-	#$newFile = Get-Item G:\MSSQL_DATA\AxDB*$dt*Primary.mdf
-	$newFile = Get-Item ("G:\MSSQL_DATA\*20220521_Primary.mdf")
+	$newFile = Get-Item G:\MSSQL_DATA\AxDB*$dt*Primary.mdf
+	#$newFile = Get-Item "G:\MSSQL_DATA\*20220521_Primary.mdf"
 	
 	$sqlprogressbaroverlay.Maximum = (Get-Item $oldFile).length/1MB
 	$sqlprogressbaroverlay.Value = 0
