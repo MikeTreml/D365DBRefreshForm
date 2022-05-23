0378bac701746a7a3f545fdf705b7fbc504667f7
Michael Treml <miketreml@mac.com>
2022-05-23 18:55:12 -0500

    Update pbar.ps1

--- pbar.ps1
+++ pbar.ps1
@@ -1,3 +1,4 @@
+
 function Show-dd_psf {
 
 	#----------------------------------------------
@@ -13,35 +14,12 @@ function Show-dd_psf {
 	[System.Windows.Forms.Application]::EnableVisualStyles()
 	$form1 = New-Object 'System.Windows.Forms.Form'
 	$label1 = New-Object 'System.Windows.Forms.Label'
-	$button1 = New-Object 'System.Windows.Forms.Button'
+	
 	$progressbar1 = New-Object 'System.Windows.Forms.ProgressBar'
 	$InitialFormWindowState = New-Object 'System.Windows.Forms.FormWindowState'
 	#endregion Generated Form Objects
 
-	#----------------------------------------------
-	# User Generated Script
-	#----------------------------------------------
 	
-	$form1_Load={
-		$file = ''
-		$progressbar1.Maximum = (Get-Item $file).length/1MB
-		$progressbar1.Value = 0
-		$timeout = new-timespan -Hours 6
-	}
-	
-	$button1_Click={
-		#TODO: Place custom script here
-		$label1.Text = [String]$($($progressbar1.Value * 100) / $progressbar1.Maximum)
-		
-		while ($progressbar1.Value -lt $progressbar1.Maximum)
-		{
-			$progressbar1.Value = $progressbar1.Value + 50
-			$label1.Text = [String]$($($progressbar1.Value * 100) / $progressbar1.Maximum)
-			start-sleep -seconds 1
-		}
-	}
-	
-	# --End User Generated Script--
 	#----------------------------------------------
 	#region Generated Events
 	#----------------------------------------------
@@ -57,7 +35,7 @@ function Show-dd_psf {
 		#Remove all event handlers from the controls
 		try
 		{
-			$button1.remove_Click($button1_Click)
+			
 			$form1.remove_Load($form1_Load)
 			$form1.remove_Load($Form_StateCorrection_Load)
 			$form1.remove_FormClosed($Form_Cleanup_FormClosed)
@@ -73,13 +51,13 @@ function Show-dd_psf {
 	#
 	# form1
 	#
-	$form1.Controls.Add($label1)
-	$form1.Controls.Add($button1)
+	#$form1.Controls.Add($label1)
+	
 	$form1.Controls.Add($progressbar1)
 	$form1.AutoScaleDimensions = New-Object System.Drawing.SizeF(6, 13)
 	$form1.AutoScaleMode = 'Font'
 	$form1.ClientSize = New-Object System.Drawing.Size(426, 60)
-	$form1.Margin = '2, 2, 2, 2'
+	$form1.Margin = '0, 0, 0, 0'
 	$form1.Name = 'form1'
 	$form1.Text = 'Form'
 	$form1.add_Load($form1_Load)
@@ -88,21 +66,12 @@ function Show-dd_psf {
 	#
 	$label1.AutoSize = $True
 	$label1.Location = New-Object System.Drawing.Point(351, 37)
-	$label1.Margin = '2, 0, 2, 0'
+	$label1.Margin = '0, 0, 0, 0'
 	$label1.Name = 'label1'
 	$label1.Size = New-Object System.Drawing.Size(35, 13)
 	$label1.TabIndex = 2
-	$label1.Text = 'label1'
-	#
-	# button1
-	#
-	$button1.Location = New-Object System.Drawing.Point(35, 57)
-	$button1.Name = 'button1'
-	$button1.Size = New-Object System.Drawing.Size(75, 23)
-	$button1.TabIndex = 1
-	$button1.Text = 'button1'
-	$button1.UseVisualStyleBackColor = $True
-	$button1.add_Click($button1_Click)
+	$label1.Text = ''
+	
 	#
 	# progressbar1
 	#
