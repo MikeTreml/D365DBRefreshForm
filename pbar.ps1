01df81182229dac5dbc07725ff928ddd97281dfe
Michael Treml <miketreml@mac.com>
2022-05-23 21:02:51 -0500

    Update pbar.ps1

--- pbar.ps1
+++ pbar.ps1
@@ -1,32 +1,106 @@
-
-function Show-dd_psf {
+function Show-bar_psf {
 
 	#----------------------------------------------
 	#region Import the Assemblies
 	#----------------------------------------------
-	[void][reflection.assembly]::Load('System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
 	[void][reflection.assembly]::Load('System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
+	[void][reflection.assembly]::Load('System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
 	#endregion Import Assemblies
+
+	#----------------------------------------------
+	#region Define SAPIEN Types
+	#----------------------------------------------
+	try{
+		[ProgressBarOverlay] | Out-Null
+	}
+	catch
+	{
+        if ($PSVersionTable.PSVersion.Major -ge 7)
+		{
+			$Assemblies = 'System.Windows.Forms', 'System.Drawing', 'System.Drawing.Primitives', 'System.ComponentModel.Primitives', 'System.Drawing.Common', 'System.Runtime'
+            if ($PSVersionTable.PSVersion.Minor -ge 1)
+			{
+				$Assemblies += 'System.Windows.Forms.Primitives'
+			}
+		}
+		else
+		{
+			$Assemblies = 'System.Windows.Forms', 'System.Drawing'  
+
+        }
+		Add-Type -ReferencedAssemblies $Assemblies -TypeDefinition @"
+		using System;
+		using System.Windows.Forms;
+		using System.Drawing;
+        namespace SAPIENTypes
+        {
+		    public class ProgressBarOverlay : System.Windows.Forms.ProgressBar
+	        {
+                public ProgressBarOverlay() : base() { SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true); }
+	            protected override void WndProc(ref Message m)
+	            { 
+	                base.WndProc(ref m);
+	                if (m.Msg == 0x000F)// WM_PAINT
+	                {
+	                    if (Style != System.Windows.Forms.ProgressBarStyle.Marquee || !string.IsNullOrEmpty(this.Text))
+                        {
+                            using (Graphics g = this.CreateGraphics())
+                            {
+                                using (StringFormat stringFormat = new StringFormat(StringFormatFlags.NoWrap))
+                                {
+                                    stringFormat.Alignment = StringAlignment.Center;
+                                    stringFormat.LineAlignment = StringAlignment.Center;
+                                    if (!string.IsNullOrEmpty(this.Text))
+                                        g.DrawString(this.Text, this.Font, Brushes.Black, this.ClientRectangle, stringFormat);
+                                    else
+                                    {
+                                        int percent = (int)(((double)Value / (double)Maximum) * 100);
+                                        g.DrawString(percent.ToString() + "%", this.Font, Brushes.Black, this.ClientRectangle, stringFormat);
+                                    }
+                                }
+                            }
+                        }
+	                }
+	            }
+              
+                public string TextOverlay
+                {
+                    get
+                    {
+                        return base.Text;
+                    }
+                    set
+                    {
+                        base.Text = value;
+                        Invalidate();
+                    }
+                }
+	        }
+        }
+"@ -IgnoreWarnings | Out-Null
+	}
+	#endregion Define SAPIEN Types
 
 	#----------------------------------------------
 	#region Generated Form Objects
 	#----------------------------------------------
 	[System.Windows.Forms.Application]::EnableVisualStyles()
-	$form1 = New-Object 'System.Windows.Forms.Form'
+	$formSQLProgress = New-Object 'System.Windows.Forms.Form'
 	$label1 = New-Object 'System.Windows.Forms.Label'
-	$progressbar1 = New-Object 'System.Windows.Forms.ProgressBar'
+	$sqlprogressbaroverlay = New-Object 'SAPIENTypes.ProgressBarOverlay'
 	$InitialFormWindowState = New-Object 'System.Windows.Forms.FormWindowState'
 	#endregion Generated Form Objects
 
+	$formSQLProgress_Load={
+		#TODO: Initialize Form Controls here
+		#Set-ControlTheme $formSQLProgress -Theme Dark
+	}
 	
-	#----------------------------------------------
-	#region Generated Events
-	#----------------------------------------------
 	
 	$Form_StateCorrection_Load=
 	{
 		#Correct the initial state of the form to prevent the .Net maximized form issue
-		$form1.WindowState = $InitialFormWindowState
+		$formSQLProgress.WindowState = $InitialFormWindowState
 	}
 	
 	$Form_Cleanup_FormClosed=
@@ -34,9 +108,9 @@ function Show-dd_psf {
 		#Remove all event handlers from the controls
 		try
 		{
-			$form1.remove_Load($form1_Load)
-			$form1.remove_Load($Form_StateCorrection_Load)
-			$form1.remove_FormClosed($Form_Cleanup_FormClosed)
+			$formSQLProgress.remove_Load($formSQLProgress_Load)
+			$formSQLProgress.remove_Load($Form_StateCorrection_Load)
+			$formSQLProgress.remove_FormClosed($Form_Cleanup_FormClosed)
 		}
 		catch { Out-Null <# Prevent PSScriptAnalyzer warning #> }
 	}
@@ -45,45 +119,47 @@ function Show-dd_psf {
 	#----------------------------------------------
 	#region Generated Form Code
 	#----------------------------------------------
-	$form1.SuspendLayout()
+	$formSQLProgress.SuspendLayout()
 	#
-	# form1
+	# formSQLProgress
 	#
-	$form1.Controls.Add($label1)
-	$form1.Controls.Add($progressbar1)
-	$form1.AutoScaleDimensions = New-Object System.Drawing.SizeF(6, 13)
-	$form1.AutoScaleMode = 'Font'
-	$form1.ClientSize = New-Object System.Drawing.Size(426, 60)
-	$form1.Margin = '0, 0, 0, 0'
-	$form1.Name = 'form1'
-	$form1.Text = 'Form'
-	$form1.add_Load($form1_Load)
-	
+	$formSQLProgress.Controls.Add($label1)
+	$formSQLProgress.Controls.Add($sqlprogressbaroverlay)
+	$formSQLProgress.AutoScaleDimensions = New-Object System.Drawing.SizeF(10, 20)
+	$formSQLProgress.AutoScaleMode = 'Font'
+	$formSQLProgress.ClientSize = New-Object System.Drawing.Size(684, 60)
+	$formSQLProgress.Name = 'formSQLProgress'
+	$formSQLProgress.Text = 'SQL Progress'
+	$formSQLProgress.add_Load($formSQLProgress_Load)
+	#
 	# label1
+	#
 	$label1.AutoSize = $True
-	$label1.Location = New-Object System.Drawing.Point(351, 37)
-	$label1.Margin = '0, 0, 0, 0'
+	$label1.Location = New-Object System.Drawing.Point(622, 21)
+	$label1.Margin = '5, 0, 5, 0'
 	$label1.Name = 'label1'
-	$label1.Size = New-Object System.Drawing.Size(35, 13)
-	$label1.TabIndex = 2
+	$label1.Size = New-Object System.Drawing.Size(53, 20)
+	$label1.TabIndex = 1
 	$label1.Text = ''
-	
-	# progressbar1
-	$progressbar1.Location = New-Object System.Drawing.Point(8, 8)
-	$progressbar1.Name = 'progressbar1'
-	$progressbar1.Size = New-Object System.Drawing.Size(413, 23)
-	$progressbar1.TabIndex = 0
-	$form1.ResumeLayout()
+	#
+	# sqlprogressbaroverlay
+	#
+	$sqlprogressbaroverlay.Location = New-Object System.Drawing.Point(14, 14)
+	$sqlprogressbaroverlay.Margin = '2, 2, 2, 2'
+	$sqlprogressbaroverlay.Name = 'sqlprogressbaroverlay'
+	$sqlprogressbaroverlay.Size = New-Object System.Drawing.Size(600, 35)
+	$sqlprogressbaroverlay.TabIndex = 0
+	$formSQLProgress.ResumeLayout()
 	#endregion Generated Form Code
 
 	#----------------------------------------------
-
+	Add-Type -AssemblyName PresentationCore,PresentationFramework
 	#Save the initial state of the form
-	$InitialFormWindowState = $form1.WindowState
+	$InitialFormWindowState = $formSQLProgress.WindowState
 	#Init the OnLoad event to correct the initial state of the form
-	$form1.add_Load($Form_StateCorrection_Load)
+	$formSQLProgress.add_Load($Form_StateCorrection_Load)
 	#Clean up the control events
-	$form1.add_FormClosed($Form_Cleanup_FormClosed)
+	$formSQLProgress.add_FormClosed($Form_Cleanup_FormClosed)
 	#Show the Form
 	$formSQLProgress.Show()
 	
@@ -93,8 +169,7 @@ function Show-dd_psf {
 	$newFile = Get-Item G:\MSSQL_DATA\AxDB*$dt*Primary.mdf
 	$sqlprogressbaroverlay.Maximum = (Get-Item $oldFile).length/1MB
 	$sqlprogressbaroverlay.Value = 0
-	[System.Windows.MessageBox]::Show($oldFile)
-	[System.Windows.MessageBox]::Show($newFile)
+	#[System.Windows.MessageBox]::Show($oldFile)
 	#[System.Windows.MessageBox]::Show($sqlprogressbaroverlay.Maximum)
 	#[System.Windows.MessageBox]::Show(($newFile).length/1MB)
 	$counter = 0
@@ -109,7 +184,8 @@ function Show-dd_psf {
 		}
 		Start-Sleep -Seconds 8;
 	}
+	
 } #End Function
 
 #Call the form
-Show-dd_psf | Out-Null
+Show-bar_psf | Out-Null
