aef5487efb04c9665b95093b678f63ce366e78fc
Michael Treml <miketreml@mac.com>
2022-09-24 00:32:31 -0500

    Update ListOutUserEmails.ps1

--- ListOutUserEmails.ps1
+++ ListOutUserEmails.ps1
@@ -1,10 +1,10 @@
- WriteLog "List Out User Email Addresses" -ForegroundColor Yellow
-  Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "
-  select ID, Name, NetworkAlias, NETWORKDOMAIN, Enable from userInfo
-  where NETWORKALIAS not like '%@contosoax7.onmicrosoft.com'
-  and NETWORKALIAS not like '%@capintegration01.onmicrosoft.com'
-  and NETWORKALIAS not like '%@devtesttie.ccsctp.net'
-  and NETWORKALIAS not like '%@DAXMDSRunner.com'
-  and NETWORKALIAS not like '%@dynamics.com'
-  and NETWORKALIAS != ''"
-  $mainprogressbaroverlay.PerformStep()
+Write-host "List Out User Email Addresses" -ForegroundColor Yellow
+Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "
+select ID, Name, NetworkAlias, NETWORKDOMAIN, Enable from userInfo
+where NETWORKALIAS not like '%@contosoax7.onmicrosoft.com'
+and NETWORKALIAS not like '%@capintegration01.onmicrosoft.com'
+and NETWORKALIAS not like '%@devtesttie.ccsctp.net'
+and NETWORKALIAS not like '%@DAXMDSRunner.com'
+and NETWORKALIAS not like '%@dynamics.com'
+and NETWORKALIAS != ''"
+
