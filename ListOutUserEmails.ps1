7d93fec83fb57eee997382f463b316f96afaa725
Michael Treml <miketreml@mac.com>
2022-09-25 21:18:19 -0500

    Update ListOutUserEmails.ps1

--- ListOutUserEmails.ps1
+++ ListOutUserEmails.ps1
@@ -6,5 +6,5 @@ and NETWORKALIAS not like '%@capintegration01.onmicrosoft.com'
 and NETWORKALIAS not like '%@devtesttie.ccsctp.net'
 and NETWORKALIAS not like '%@DAXMDSRunner.com'
 and NETWORKALIAS not like '%@dynamics.com'
-and NETWORKALIAS != ''"
+and NETWORKALIAS != ''" | Out-File -FilePath $Logfile
 
