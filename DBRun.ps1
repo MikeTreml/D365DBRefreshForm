833e72e1106fd192d745f4f59695c0df86ec18d8
Michael Treml <miketreml@mac.com>
2022-09-25 20:57:19 -0500

    Update DBRun.ps1

--- DBRun.ps1
+++ DBRun.ps1
@@ -1,16 +1,15 @@
 $ErrorActionPreference = 'Inquire'
 
-$mainprogressbaroverlay.Maximum = 14
+$mainprogressbaroverlay.Maximum = 11
 $mainprogressbaroverlay.Step = 1
 $mainprogressbaroverlay.Value = 0
 $mainprogressbaroverlay.Visible = $True
-$NewDB = 'AxDB' #Database name. No spaces in the name!
-$renameOldFile = $('G:\MSSQL_DATA\AxDB_PrimaryOld_') + $dt + $('.mdf')
-[string]$dt = get-date -Format "yyyyMMdd" #Generate the datetime stamp to make DB files unique
+$NewDB = 'AxDB' 
+[string]$dt = get-date -Format "yyyyMMdd" 
 count-checkbox
 
-WriteLog "Old File Name  - " $oldFile = Get-Item G:\MSSQL_DATA\AxDB*Primary.mdf -Verbose
-Write-Host "Old File Name  - " $oldFile -ForegroundColor Yellow
+#WriteLog "Old File Name  - " $oldFile = Get-Item G:\MSSQL_DATA\AxDB*Primary.mdf -Verbose
+#Write-Host "Old File Name  - " $oldFile -ForegroundColor Yellow
 WriteLog "Rename old File  - " $renameOldFile -ForegroundColor Yellow
 Write-Host "Rename old File - " $renameOldFile -ForegroundColor Yellow
 
@@ -60,7 +59,7 @@ Write-Host "Stopping D365FO environment" -ForegroundColor Yellow
 Stop-D365Environment -All -Kill -Verbose
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Stopping D365FO environment"
-Write-Host "Stopping D365FO environment" -ForegroundColor Yellow
+Write-Host "Stopping D365FO environment" -ForegroundColor Green
 
 
 WriteLog "truncate"
@@ -68,27 +67,27 @@ Write-Host "truncate" -ForegroundColor Yellow
 Clear-D365BacpacTableData -Path "D:\Temp\AxDB.bacpac" -Table "dbo.BATCHHISTORY", "BATCHJOBHISTORY", "SYSSERVERCONFIG", "SYSSERVERSESSIONS", "SYSCORPNETPRINTERS", "SYSCLIENTSESSIONS", "BATCHSERVERCONFIG", "BATCHSERVERGROUP" -ClearFromSource -Verbose
 $mainprogressbaroverlay.PerformStep()
 WriteLog "truncate"
-Write-Host "truncate" -ForegroundColor Yellow
+Write-Host "truncate" -ForegroundColor Green
 
 
 WriteLog "Enable-D365Exception"
-Write-Host  "Enable-D365Exception"
+Write-Host  "Enable-D365Exception" -ForegroundColor Yellow
 Enable-D365Exception -Verbose
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Enable-D365Exception"
-Write-Host "Enable-D365Exception"
+Write-Host "Enable-D365Exception" -ForegroundColor Green
 
 
 WriteLog "Installing modern SqlPackage"
-Write-Host  "Installing modern SqlPackage"
-Invoke-D365InstallSqlPackage -Verbose #Installing modern SqlPackage just in case  
+Write-Host  "Installing modern SqlPackage" -ForegroundColor Yellow
+Invoke-D365InstallSqlPackage -Verbose 
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Installing modern SqlPackage"
-Write-Host  "Installing modern SqlPackage"
+Write-Host  "Installing modern SqlPackage" -ForegroundColor Green
 
 
 WriteLog "Checking SQL file"
-Write-Host  "Checking SQL file"
+Write-Host  "Checking SQL file" -ForegroundColor Yellow
 If (-not (Test-DbaPath -SqlInstance localhost -Path $($f.FullName)))
 {
 	Write-Warning "Database file $($f.FullName) could not be found by SQL Server. Try to move it to C:\Temp or D:\Temp"
@@ -96,88 +95,54 @@ If (-not (Test-DbaPath -SqlInstance localhost -Path $($f.FullName)))
 }
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Checking SQL file"
-Write-Host  "Checking SQL file"
+Write-Host  "Checking SQL file" -ForegroundColor Green
 
 
 WriteLog "Unblock-File"
-Write-Host  "Unblock-File"
+Write-Host  "Unblock-File" -ForegroundColor Yellow
 $f | Unblock-File
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Unblock-File"
-Write-Host  "Unblock-File"
+Write-Host  "Unblock-File" -ForegroundColor Green
 
 
 WriteLog "Import-D365Bacpac"
-Write-Host  "Import-D365Bacpac"
+Write-Host  "Import-D365Bacpac" -ForegroundColor Yellow
 Import-D365Bacpac -ImportModeTier1 -BacpacFile $f.FullName -NewDatabaseName $NewDB -ShowOriginalProgress
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Import-D365Bacpac"
-Write-Host  "Import-D365Bacpac"
+Write-Host  "Import-D365Bacpac" -ForegroundColor Green
 
-
-## Removing AxDB_orig database and Switching AxDB:   NULL <-1- AxDB_original <-2- AxDB <-3- [NewDB]
-WriteLog "Stopping D365FO environment and Switching Databases" -ForegroundColor Yellow
-Write-Host  "Stopping D365FO environment and Switching Databases" -ForegroundColor Yellow
-Stop-D365Environment -All -Kill -Verbose
-$mainprogressbaroverlay.PerformStep()
-WriteLog "Stopping D365FO environment and Switching Databases" -ForegroundColor Yellow
-Write-Host  "Stopping D365FO environment and Switching Databases" -ForegroundColor Yellow
+## Stopping enviroment again before switch
+#WriteLog "Stopping D365FO environment and Switching Databases" 
+#Write-Host  "Stopping D365FO environment and Switching Databases" -ForegroundColor Yellow
+#Stop-D365Environment -All -Kill -Verbose
+#WriteLog "Stopping D365FO environment and Switching Databases" 
+#Write-Host  "Stopping D365FO environment and Switching Databases" -ForegroundColor Green
 
 
 WriteLog "Switch-D365ActiveDatabase"
-Write-Host  "Switch-D365ActiveDatabase"
+Write-Host  "Switch-D365ActiveDatabase" -ForegroundColor Yellow
 Switch-D365ActiveDatabase -NewDatabaseName $NewDB -Verbose
 $mainprogressbaroverlay.PerformStep()
 WriteLog "Switch-D365ActiveDatabase"
-Write-Host  "Switch-D365ActiveDatabase"
+Write-Host  "Switch-D365ActiveDatabase" -ForegroundColor Green
+
+### Manual remove is a better option
+#WriteLog "Remove-D365Database"
+#Write-Host  "Remove-D365Database" -ForegroundColor Yellow
+#Remove-D365Database -DatabaseName 'AxDB_Original' -Verbose
+#WriteLog "Remove-D365Database"
+#Write-Host  "Remove-D365Database" -ForegroundColor Green
 
 
-WriteLog "Remove-D365Database"
-Write-Host  "Remove-D365Database"
-Remove-D365Database -DatabaseName 'AxDB_Original' -Verbose
-$mainprogressbaroverlay.PerformStep()
-WriteLog "Remove-D365Database"
-Write-Host  "Remove-D365Database"
-
-
-WriteLog "Starting D365FO environment. Then open UI and refresh Data Entities." -ForegroundColor Yellow
+WriteLog "Starting D365FO environment. Then open UI and refresh Data Entities." 
 Write-Host  "Starting D365FO environment. Then open UI and refresh Data Entities." -ForegroundColor Yellow
 Start-D365Environment
 $mainprogressbaroverlay.PerformStep()
-WriteLog "Starting D365FO environment. Then open UI and refresh Data Entities." -ForegroundColor Yellow
-Write-Host  "Starting D365FO environment. Then open UI and refresh Data Entities." -ForegroundColor Yellow
+WriteLog "Starting D365FO environment. Then open UI and refresh Data Entities." 
+Write-Host  "Starting D365FO environment. Then open UI and refresh Data Entities." -ForegroundColor Green
 
-
-
-WriteLog "Stop-Service MSSQLSERVER, SQLSERVERAGENT"
-Write-Host  "Stop-Service MSSQLSERVER, SQLSERVERAGENT"
-Stop-Service MSSQLSERVER, SQLSERVERAGENT -Force -Verbose
-WriteLog "Stop-Service MSSQLSERVER, SQLSERVERAGENT"
-Write-Host  "Stop-Service MSSQLSERVER, SQLSERVERAGENT"
-
-[string]$dt = get-date -Format "yyyyMMdd"
-$newFile = Get-Item G:\MSSQL_DATA\AxDB*$dt*Primary.mdf
-[string]$oldFile = Get-Item 'G:\MSSQL_DATA\AxDB*Primary.mdf' -Exclude AxDB*$dt*Primary.mdf
-
-
-if ($oldFile -ne '')
-{
-	WriteLog "Move-Item -Path $oldFile -Destination G:\MSSQL_DATA\AxDB_Primaryold_" + $dt
-	Write-Host  "Move-Item -Path $oldFile -Destination G:\MSSQL_DATA\AxDB_Primaryold_" + $dt
-	Move-Item -Path $oldFile -Destination G:\MSSQL_DATA\AxDB_Primaryold_$dt.mdf
-	#Remove-D365Database -DatabaseName 'AxDB_Original'
-    WriteLog "Move-Item -Path $oldFile -Destination G:\MSSQL_DATA\AxDB_Primaryold_" + $dt
-	Write-Host  "Move-Item -Path $oldFile -Destination G:\MSSQL_DATA\AxDB_Primaryold_" + $dt
-}
-
-
-#
-WriteLog "Start-Service MSSQLSERVER, SQLSERVERAGENT "
-Write-Host  "Start-Service MSSQLSERVER, SQLSERVERAGENT "
-Start-Service MSSQLSERVER, SQLSERVERAGENT -Verbose
-$mainprogressbaroverlay.PerformStep()
-WriteLog "Start-Service MSSQLSERVER, SQLSERVERAGENT "
-Write-Host  "Start-Service MSSQLSERVER, SQLSERVERAGENT "
 
 if ($checkbox1.Checked)
 {
@@ -185,7 +150,7 @@ if ($checkbox1.Checked)
 	WriteLog "Starting 1"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done 1"
+	Write-Host "Done 1" -ForegroundColor Green
 	WriteLog "Done 1"
 }
 if ($checkbox2.Checked)
@@ -194,7 +159,7 @@ if ($checkbox2.Checked)
 	WriteLog "Starting 2"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/2)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done 2"
+	Write-Host "Done 2" -ForegroundColor Green
 	WriteLog "Done 2"
 }
 if ($checkbox3.Checked)
@@ -203,7 +168,7 @@ if ($checkbox3.Checked)
 	WriteLog "Starting 3"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/3)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done 3"
+	Write-Host "Done 3" -ForegroundColor Green
 	WriteLog "Done 3"
 }
 if ($checkbox4.Checked)
@@ -212,7 +177,7 @@ if ($checkbox4.Checked)
 	WriteLog "Starting 4"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/4)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done 4" -ForegroundColor Yellow
+	Write-Host "Done 4"  -ForegroundColor Green
 	WriteLog "Done 4"
 }
 if ($checkbox5.Checked)
@@ -221,7 +186,7 @@ if ($checkbox5.Checked)
 	WriteLog "Starting 5"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/5)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done 5"
+	Write-Host "Done 5" -ForegroundColor Green
 	WriteLog "Done 5"
 }
 if ($checkboxBackupNewlyCompleted.Checked)
@@ -230,7 +195,7 @@ if ($checkboxBackupNewlyCompleted.Checked)
 	WriteLog "Starting Backup AxDB"
 	Invoke-Expression $(Invoke-WebRequest  https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/BackUpDB.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done Backup AxDB"
+	Write-Host "Done Backup AxDB" -ForegroundColor Green
 	WriteLog "Done Backup AxDB"
 }
 
@@ -240,84 +205,84 @@ if ($checkboxCleanUpPowerBISettin.Checked)
 	WriteLog "Starting Cleaning up Power BI settings"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/CleanPowerBI.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done Cleaning up Power BI settings"
+	Write-Host "Done Cleaning up Power BI settings" -ForegroundColor Green
 	WriteLog "Done Cleaning up Power BI settings"
 }
 
 if ($checkboxEnableSQLChangeTrack.Checked)
 {
-	Write-Host "Starting SQL Tracking"
+	Write-Host "Starting SQL Tracking" -ForegroundColor Yellow
 	WriteLog "Starting SQL Tracking"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/SQLTracking.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done SQL Tracking"
+	Write-Host "Done SQL Tracking" -ForegroundColor Green
 	WriteLog "Done SQL Tracking"
 }
 
 if ($checkboxPromoteNewAdmin.Checked)
 {
-	Write-Host "Starting New Admin"
+	Write-Host "Starting New Admin" -ForegroundColor Yellow
 	WriteLog "Starting New Admin"
 	Invoke-Expression $(Invoke-WebRequest  https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/NewAdmin.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done New Admin"
+	Write-Host "Done New Admin" -ForegroundColor Green
 	WriteLog "Done New Admin"
 }
 
 if ($checkboxTruncateBatchTables.Checked)
 {
-	Write-Host "Starting truncate Batch"
+	Write-Host "Starting truncate Batch" -ForegroundColor Yellow
 	WriteLog "Starting truncate Batch"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/TruncateBatch.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done truncate Batch"
+	Write-Host "Done truncate Batch" -ForegroundColor Green
 	WriteLog "Done truncate Batch"
 }
 
 if ($checkboxPutAllBatchJobsOnHol.Checked)
 {
-	Write-Host "Starting hold batch jobs"
+	Write-Host "Starting hold batch jobs" -ForegroundColor Yellow
 	WriteLog "Starting hold batch jobs"
 	Invoke-Expression $(Invoke-WebRequest  https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/BatchHold.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done hold batch jobs"
+	Write-Host "Done hold batch jobs" -ForegroundColor Green
 	WriteLog "Done hold batch jobs"
 }
 
 if ($checkboxRunDatabaseSync.Checked)
 {
-	Write-Host "Starting DB sync"
+	Write-Host "Starting DB sync" -ForegroundColor Yellow
 	WriteLog "Starting DB sync"
 	Invoke-Expression $(Invoke-WebRequest  https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/DBSync.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done DB sync"
+	Write-Host "Done DB sync" -ForegroundColor Green
 	WriteLog "Done DB sync"
 }
 
 if ($checkboxSetDBRecoveryModel.Checked)
 {
-	Write-Host  "Starting "
+	Write-Host  "Starting " -ForegroundColor Yellow
 	WriteLog "Starting "
 	Invoke-Expression $(Invoke-WebRequest  https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/SetDBRecoveryModel.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done"
+	Write-Host "Done" -ForegroundColor Green
 	WriteLog "Done"
 }
 
 
 if ($checkboxEnableUsersExceptGue.Checked)
 {
-	Write-Host "Starting Enable Users"
+	Write-Host "Starting Enable Users" -ForegroundColor Yellow
 	WriteLog "Starting Enable Users"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/EnableUsers.ps1)
 	$mainprogressbaroverlay.PerformStep()
-	Write-Host "Done Enable Users"
+	Write-Host "Done Enable Users" -ForegroundColor Green
 	WriteLog "Done Enable Users"
 }
 
 if ($checkboxListOutUserEmails.Checked)
 {
-	Write-Host "Starting List Out User Email Addresses"
+	Write-Host "Starting List Out User Email Addresses" -ForegroundColor Yellow
 	WriteLog "Starting List Out User Email Addresses"
 	Invoke-Expression $(Invoke-WebRequest https://raw.githubusercontent.com/MikeTreml/D365DBRefreshForm/main/ListOutUserEmails.ps1)
 	$mainprogressbaroverlay.PerformStep()
