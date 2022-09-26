264e1ca256015ff9d94b1764acca1f5b08d8cd88
Michael Treml <miketreml@mac.com>
2022-09-25 21:17:40 -0500

    Update EnableUsers.ps1

--- EnableUsers.ps1
+++ EnableUsers.ps1
@@ -1,6 +1,6 @@
 ## Enable Users except Guest
   WriteLog "Enable all users except Guest" -ForegroundColor Yellow
-  #Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "Update USERINFO set ENABLE = 1 where ID != 'Guest'"
-  Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "Update USERINFO set ENABLE = 1 where ID != 'Guest'"
-  Update-D365User
+  Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "Update USERINFO set ENABLE = 1 where ID != 'Guest'"  | Out-File -FilePath $Logfile
+  #Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "Update USERINFO set ENABLE = 1 where ID != 'Guest'"
+  Update-D365User -email "*@caf2code.com"
   $mainprogressbaroverlay.PerformStep()
