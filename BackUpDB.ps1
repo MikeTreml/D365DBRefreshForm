8f2e2e5790777f3d055337252c2916c7e5b7460f
Michael Treml <miketreml@mac.com>
2022-09-30 13:43:28 -0500

    Update BackUpDB.ps1

--- BackUpDB.ps1
+++ BackUpDB.ps1
@@ -1,6 +1,4 @@
  ## Backup AxDB database
-
-  WriteLog "Backup AxDB" -ForegroundColor Yellow
-  $labelInfo = ""
-  Invoke-DbaQuery -Verbose -SqlInstance localhost -Database AxDB -Type Full -CompressBackup -BackupFileName "dbname-$NewDB-backuptype-timestamp.bak" -ReplaceInName | Out-File -FilePath $Logfile
-  $mainprogressbaroverlay.PerformStep()
+$labelInfo = ""
+Invoke-DbaQuery -Verbose -SqlInstance localhost -Database AxDB -Type Full -CompressBackup -BackupFileName "dbname-$NewDB-backuptype-timestamp.bak" -ReplaceInName | Out-File -FilePath $Logfile
+  
