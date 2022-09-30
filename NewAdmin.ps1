4eac6177f9e3fb477b9e475ee65b1a631afc043e
Michael Treml <miketreml@mac.com>
2022-09-30 13:55:31 -0500

    Update NewAdmin.ps1

--- NewAdmin.ps1
+++ NewAdmin.ps1
@@ -1,4 +1,3 @@
  ## Promote user as admin and set default tenant  (Optional)
-  WriteLog "Setting up new Admin" -ForegroundColor Yellow
   Set-D365Admin -Verbose -AdminSignInName textboxAdminEmailAddress.Text
-  $mainprogressbaroverlay.PerformStep()
+
