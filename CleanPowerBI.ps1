020763f5442496e31b45e9fb8905d039c797bfb0
Michael Treml <miketreml@mac.com>
2022-09-30 13:44:55 -0500

    Update CleanPowerBI.ps1

--- CleanPowerBI.ps1
+++ CleanPowerBI.ps1
@@ -1,4 +1,3 @@
 ## Clean up Power BI settings
-  WriteLog "Cleaning up Power BI settings" -ForegroundColor Yellow
-  Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"  | Out-File -FilePath $Logfile
-  $mainprogressbaroverlay.PerformStep()
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"  | Out-File -FilePath $Logfile
+
