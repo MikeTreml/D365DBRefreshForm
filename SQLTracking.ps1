62aec537ede984b736e0e3e2216a57e456fcf190
Michael Treml <miketreml@mac.com>
2022-09-30 13:55:51 -0500

    Update SQLTracking.ps1

--- SQLTracking.ps1
+++ SQLTracking.ps1
@@ -1,4 +1,3 @@
  ## Enable SQL Change Tracking
-  WriteLog "Enabling SQL Change Tracking" -ForegroundColor Yellow
   Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "ALTER DATABASE AxDB SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)" | Out-File -FilePath $Logfile
 
