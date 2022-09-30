265df7153eeba7467dc2618ecd851bbd69018b3f
Michael Treml <miketreml@mac.com>
2022-09-30 13:44:36 -0500

    Update BatchHold.ps1

--- BatchHold.ps1
+++ BatchHold.ps1
@@ -1,5 +1,4 @@
  ## Put on hold all Batch Jobs
-  WriteLog "Disabling all current Batch Jobs" -ForegroundColor Yellow
-  #Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)  --Set any waiting, executing, ready, or canceling batches to withhold."
-  Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7) | Out-File -FilePath $Logfile
-  $mainprogressbaroverlay.PerformStep()
+  Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)  --Set any waiting, executing, ready, or canceling batches to withhold."
+  #Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7) | Out-File -FilePath $Logfile
+
