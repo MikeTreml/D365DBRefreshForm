4746a337e4c8e31d34a04b95c1236b2b3375f208
Michael Treml <miketreml@mac.com>
2022-10-26 11:34:27 -0500

    Update ListOutUserEmails.ps1

--- ListOutUserEmails.ps1
+++ ListOutUserEmails.ps1
@@ -1,3 +1,7 @@
+$Stamp = (Get-Date).toString("yyyy-MM-dd")
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
+Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
+
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "
 select ID, Name, NetworkAlias, NETWORKDOMAIN, Enable from userInfo
 where NETWORKALIAS not like '%@contosoax7.onmicrosoft.com'
@@ -7,3 +11,4 @@ and NETWORKALIAS not like '%@DAXMDSRunner.com'
 and NETWORKALIAS not like '%@dynamics.com'
 and NETWORKALIAS != ''" | Out-File -FilePath $Logfile
 
+Stop-Transcript
