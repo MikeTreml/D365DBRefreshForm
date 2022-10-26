1d87b20291fcd013d576fc834bb4dfa2f260ec76
Michael Treml <miketreml@mac.com>
2022-10-26 11:12:53 -0500

    Update Remove-D365Database.ps1

--- Remove-D365Database.ps1
+++ Remove-D365Database.ps1
@@ -1,4 +1,7 @@
+Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
 
 Stop-D365Environment -All -Kill -Verbose
 Remove-D365Database -DatabaseName 'AxDB_Original' -Verbose
 Start-D365Environment -All
+
+Stop-Transcript
