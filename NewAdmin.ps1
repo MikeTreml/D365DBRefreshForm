db480e4fd6e71206ca58cb965095e99617dec68b
Michael Treml <miketreml@mac.com>
2022-10-26 11:35:27 -0500

    Update NewAdmin.ps1

--- NewAdmin.ps1
+++ NewAdmin.ps1
@@ -1,3 +1,8 @@
- ## Promote user as admin and set default tenant  (Optional)
-  Set-D365Admin -Verbose -AdminSignInName textboxAdminEmailAddress.Text
+$Stamp = (Get-Date).toString("yyyy-MM-dd")
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
+Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
 
+## Promote user as admin and set default tenant  (Optional)
+Set-D365Admin -Verbose -AdminSignInName textboxAdminEmailAddress.Text
+
+Stop-Transcript 
