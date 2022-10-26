bc473e6aafeffd8c3c52e235fe851993507f6cbd
Michael Treml <miketreml@mac.com>
2022-10-25 21:54:41 -0500

    Update D365tools.ps1

--- D365tools.ps1
+++ D365tools.ps1
@@ -6,17 +6,14 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 $modules2Install = @('d365fo.tools', 'dbatools')
 foreach ($module in $modules2Install)
 {
-  WriteLog "..working on module $module"
   Write-Host  "..working on module $module"
   if ($null -eq $(Get-Command -Module $module))
   {
-    WriteLog "....installing module $module" -ForegroundColor Gray
     Write-Host  "....installing module $module" -ForegroundColor Gray
     Install-Module -Name $module -SkipPublisherCheck -Scope AllUsers -Verbose
   }
   else
   {
-    WriteLog "....updating module" $module -ForegroundColor Gray
     Write-Host  "....updating module" $module -ForegroundColor Gray
     Update-Module -Name $module -Verbose
   }
