bf954d30f25f99e95d042d1423f73d0d383c4a90
Michael Treml <miketreml@mac.com>
2022-10-26 12:01:49 -0500

    Update TruncateBatch.ps1

--- TruncateBatch.ps1
+++ TruncateBatch.ps1
@@ -18,4 +18,18 @@ TRUNCATE TABLE DMFDEFINITIONGROUPEXECUTIONHISTORY
 TRUNCATE TABLE DMFEXECUTION
 TRUNCATE TABLE DMFDEFINITIONGROUPEXECUTION" 
 
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "declare @t table 
+(id bigint identity(1,1), sqlQuery nvarchar(500))
+declare @maxid bigint, @i	bigint, @sqlquery nvarchar(500)
+insert into @t (sqlQuery)
+select ' truncate table ' + name from sysobjects where name like '%staging'
+select @maxid = MAX(id) from @t
+set @i=1
+while (@i<=@maxid)
+BEGIN
+select @sqlquery = sqlQuery from @t where id = @i
+exec(@sqlQuery)
+set @i=@i+1
+END"
+
 Stop-Transcript 
