1a8541e7e3c7caf902f38b5b44db72c6ebfcf3cd
Michael Treml <miketreml@mac.com>
2022-10-29 21:56:48 -0500

    Update BackUpDB.ps1

--- BackUpDB.ps1
+++ BackUpDB.ps1
@@ -1,6 +1,6 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
 $LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
-Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
+Start-Transcript -Path $LogFile -Append -UseMinimalHeader -Force
 
 $labelInfo = ""
 Invoke-DbaQuery -Verbose -SqlInstance localhost -Database AxDB -Type Full -CompressBackup -BackupFileName "dbname-$NewDB-backuptype-timestamp.bak" -ReplaceInName | Out-File -FilePath $Logfile
