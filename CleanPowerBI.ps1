748a57e964028e4cde8887befd660a0fdee299e5
Michael Treml <miketreml@mac.com>
2022-10-29 21:58:22 -0500

    Update CleanPowerBI.ps1

--- CleanPowerBI.ps1
+++ CleanPowerBI.ps1
@@ -1,6 +1,6 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
 $LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
-Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
+Start-Transcript -Path $LogFile -Append -UseMinimalHeader -Force
 
 ## Clean up Power BI settings
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"  | Out-File -FilePath $Logfile
