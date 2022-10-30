1f867883365ca1090519a95f95ed046dedc85dd6
Michael Treml <miketreml@mac.com>
2022-10-29 22:26:12 -0500

    Update EnableUsers.ps1

--- EnableUsers.ps1
+++ EnableUsers.ps1
@@ -1,6 +1,6 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
-Start-Transcript -Path $LogFile -Append -UseMinimalHeader -Force
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+Start-Transcript -Path $LogFile -Append -Force
 
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
 VALUES ('aniela','aniela',1,'PQI','https://sts.windows.net/caf2code.com/','aniela@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
