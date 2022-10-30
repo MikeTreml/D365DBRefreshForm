64ec71a21691128cdb56c122781854657ff41d29
Michael Treml <miketreml@mac.com>
2022-10-29 21:58:05 -0500

    Update BatchHold.ps1

--- BatchHold.ps1
+++ BatchHold.ps1
@@ -1,6 +1,6 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
 $LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
-Start-Transcript -Path $LogFile -Append -IncludeInvocationHeader
+Start-Transcript -Path $LogFile -Append -UseMinimalHeader -Force
 
 ## Put on hold all Batch Jobs
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)  --Set any waiting, executing, ready, or canceling batches to withhold."
