e8a3cdae18b7b31ed3d722d34fdaf7bb07aa5408
Michael Treml <miketreml@mac.com>
2022-10-29 22:27:28 -0500

    Update TruncateBatch.ps1

--- TruncateBatch.ps1
+++ TruncateBatch.ps1
@@ -1,6 +1,6 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp"
-Start-Transcript -Path $LogFile -Append -UseMinimalHeader -Force
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+Start-Transcript -Path $LogFile -Append -Force
 
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "TRUNCATE TABLE SYSSERVERCONFIG
 TRUNCATE TABLE SYSSERVERSESSIONS
