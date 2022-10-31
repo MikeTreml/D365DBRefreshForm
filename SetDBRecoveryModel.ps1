81e6dbffe343bd0ea0c0ef77f1edab3ed09f1cf1
Michael Treml <miketreml@mac.com>
2022-10-30 21:54:54 -0500

    Update SetDBRecoveryModel.ps1

--- SetDBRecoveryModel.ps1
+++ SetDBRecoveryModel.ps1
@@ -1,5 +1,5 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\SetDBRecoveryModel.txt"
 Start-Transcript -Path $LogFile -Append -Force
 
 Set-DbaDbRecoveryModel -Verbose -SqlInstance localhost -RecoveryModel Simple -Database AxDB -Confirm:$false
