af8cdad2e59769f87a144521249ee4021e219437
Michael Treml <miketreml@mac.com>
2022-10-30 21:55:24 -0500

    Update SQLTracking.ps1

--- SQLTracking.ps1
+++ SQLTracking.ps1
@@ -1,5 +1,5 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\SQLTracking.txt"
 Start-Transcript -Path $LogFile -Append -Force
 
 ## Enable SQL Change Tracking
