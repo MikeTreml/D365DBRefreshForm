ca7249465eb4ed3c0bde0554781d55b80b526609
Michael Treml <miketreml@mac.com>
2022-10-30 21:56:38 -0500

    Update NewAdmin.ps1

--- NewAdmin.ps1
+++ NewAdmin.ps1
@@ -1,5 +1,5 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\NewAdmin.txt"
 Start-Transcript -Path $LogFile -Append -Force
 
 ## Promote user as admin and set default tenant  (Optional)
