e4380a67146098ba4aeddf20bfaa962336d0bdf1
Michael Treml <miketreml@mac.com>
2022-10-30 20:50:48 -0500

    Update BatchHold.ps1

--- BatchHold.ps1
+++ BatchHold.ps1
@@ -1,5 +1,5 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp Batch Hold.txt"
 Start-Transcript -Path $LogFile -Append -Force
 
 ## Put on hold all Batch Jobs
