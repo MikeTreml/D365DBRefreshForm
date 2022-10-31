d71a2ee68ecfdc3eb7e179caa9aa98cb8376232f
Michael Treml <miketreml@mac.com>
2022-10-30 21:55:57 -0500

    Update Remove-D365Database.ps1

--- Remove-D365Database.ps1
+++ Remove-D365Database.ps1
@@ -1,5 +1,5 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp.txt"
+$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\Remove-D365Database..txt"
 Start-Transcript -Path $LogFile -Append -Force
 
 Stop-D365Environment -All -Kill -Verbose
