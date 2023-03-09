190c802c2dd1be1575b14f0f974578aa0a6d3a86
Michael Treml <miketreml@mac.com>
2023-03-08 20:38:32 -0600

    Update BatchHold.ps1

--- BatchHold.ps1
+++ BatchHold.ps1
@@ -1,9 +1,4 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\BatchHold.txt"
-Start-Transcript -Path $LogFile -Append -Force
 
 ## Put on hold all Batch Jobs
-Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)  --Set any waiting, executing, ready, or canceling batches to withhold."
-#Invoke-D365SqlScript -Verbose -DatabaseServer localhost -DatabaseName AxDB -Command "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7) | Out-File -FilePath $Logfile
-
-Stop-Transcript
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)"
