6a5925b209e297c51d0e2bc83c67de95dfe08310
Michael Treml <miketreml@mac.com>
2023-03-08 20:56:47 -0600

    Update SQLTracking.ps1

--- SQLTracking.ps1
+++ SQLTracking.ps1
@@ -1,8 +1,2 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\SQLTracking.txt"
-Start-Transcript -Path $LogFile -Append -Force
-
 ## Enable SQL Change Tracking
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "ALTER DATABASE AxDB SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)" | Out-File -FilePath $Logfile
-
-Stop-Transcript 
