0b852efa37830aba43a12460b67436778fb44ca5
Michael Treml <miketreml@mac.com>
2023-03-08 20:39:47 -0600

    Update CleanPowerBI.ps1

--- CleanPowerBI.ps1
+++ CleanPowerBI.ps1
@@ -1,8 +1,2 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\PowerBIClean.txt"
-Start-Transcript -Path $LogFile -Append -Force
-
 ## Clean up Power BI settings
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"  | Out-File -FilePath $Logfile
-
-Stop-Transcript
