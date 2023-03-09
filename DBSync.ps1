e89397144273d528b1f17b45e2a2faeec325a77e
Michael Treml <miketreml@mac.com>
2023-03-08 20:51:04 -0600

    Update DBSync.ps1

--- DBSync.ps1
+++ DBSync.ps1
@@ -1,8 +1,2 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\DBSync.txt"
-Start-Transcript -Path $LogFile -Append -Force
-
 ## Run Database Sync
 Invoke-D365DBSync -ShowOriginalProgress -Verbose
-
-Stop-Transcript
