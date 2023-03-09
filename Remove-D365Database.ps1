d21df21f8ed8a119414011d7de773a6f03448926
Michael Treml <miketreml@mac.com>
2023-03-08 20:56:27 -0600

    Update Remove-D365Database.ps1

--- Remove-D365Database.ps1
+++ Remove-D365Database.ps1
@@ -1,9 +1,2 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\Remove-D365Database..txt"
-Start-Transcript -Path $LogFile -Append -Force
-
 Stop-D365Environment -All -Kill -Verbose
 Remove-D365Database -DatabaseName 'AxDB_Original' -Verbose
-
-
-Stop-Transcript
