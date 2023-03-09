a8b07ef9809662bf8ddd0b8225d54f8edd75434b
Michael Treml <miketreml@mac.com>
2023-03-08 20:55:00 -0600

    Update NewAdmin.ps1

--- NewAdmin.ps1
+++ NewAdmin.ps1
@@ -1,8 +1,2 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-$LogFile = "C:\Users\$env:UserName\Desktop\DBRefresh_$Stamp\NewAdmin.txt"
-Start-Transcript -Path $LogFile -Append -Force
-
 ## Promote user as admin and set default tenant  (Optional)
 Set-D365Admin -Verbose -AdminSignInName textboxAdminEmailAddress.Text
-
-Stop-Transcript 
