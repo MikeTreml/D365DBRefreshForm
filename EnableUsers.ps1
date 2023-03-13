80d7fbafe6562f774d02c62e15e75e5a8b273636
Michael Treml <miketreml@mac.com>
2023-03-12 21:27:20 -0500

    Update EnableUsers.ps1

--- EnableUsers.ps1
+++ EnableUsers.ps1
@@ -1,40 +1,29 @@
-Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES ('aniela','aniela',1,'','https://sts.windows.net/caf2code.com/','aniela@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('mike','mike',1,'','https://sts.windows.net/caf2code.com/','mike@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('benbreeden','benbreeden',1,'','https://sts.windows.net/caf2code.com/','benbreeden@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('Brittany','Brittany',1,'','https://sts.windows.net/caf2code.com/','Brittany@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('d.ki21','d.ki21',1,'','https://sts.windows.net/caf2code.com/','d.ki21@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('L.FL21','L.FL21',1,'','https://sts.windows.net/caf2code.com/','L.FL21@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('laurenwooll','laurenwooll',1,'','https://sts.windows.net/caf2code.com/','laurenwooll@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('m.ri21','m.ri21',1,'','https://sts.windows.net/caf2code.com/','m.ri21@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('mac','mac',1,'','https://sts.windows.net/caf2code.com/','mac@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('r.qu21','r.qu21',1,'','https://sts.windows.net/caf2code.com/','r.qu21@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('aniela','aniela',1,'','https://sts.windows.net/caf2code.com/','aniela@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-INSERT INTO [AxDB].[dbo].[USERINFO]([ID],[NAME],[ENABLE],[COMPANY],[NETWORKDOMAIN],[NETWORKALIAS],[ENABLEDONCE],[LANGUAGE],[HELPLANGUAGE],[PREFERREDTIMEZONE],[ACCOUNTTYPE],[DEFAULTPARTITION],[EXTERNALIDTYPE],[INTERACTIVELOGON])
-VALUES	('justin','justin',1,'','https://sts.windows.net/caf2code.com/','justin@caf2code.com',1,'en-us','en-us',29,2,1,1,1)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('mike', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('benbreeden', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('Brittany', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('d.ki21', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('L.FL21', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('laurenwooll', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('m.ri21', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('mac', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('r.qu21', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('justin', 2, 1, 1, 0, 0)
-insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('aniela', 2, 1, 1, 0, 0)
-UPDATE [dbo].[USERINFO] SET [COMPANY] = 'PQI' WHERE NETWORKALIAS like '%caf2code.com'
-Update USERINFO set ENABLE = 1 where ID != 'Guest'"
+$co = Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query " SELECT count(COMPANY) as c, COMPANY FROM USERINFO group by COMPANY  order by c desc"
+		
+$roleId = Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "SELECT RECID FROM SECURITYROLE where name = 'System administrator'"
 
+Import-D365ExternalUser -Id "Aniela" -Name "Aniela" -Email "aniela@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "Mike" -Name "Mike" -Email "mike@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "Ben" -Name "Ben" -Email "ben@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "Brittany" -Name "Brittany" -Email "Brittany@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "d.ki21" -Name "d.ki21" -Email "d.ki21@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "L.FL21" -Name "L.FL21" -Email "L.FL21@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "Mac" -Name "Mac" -Email "mac@caf2code.com" -Company $co.company[0] -Enabled 1
+Import-D365ExternalUser -Id "Justin" -Name "Justin" -Email "justin@caf2code" -Company $co.company[0] -Enabled 1
+
+$line2 = ' insert into securityuserrole(USER_, SECURITYROLE, ASSIGNMENTSTATUS, ASSIGNMENTMODE, VALIDFROMTZID, VALIDTOTZID) values('
+
+$str = "{2}'mike', {1}, 1, 1, 0, 0)
+    {2}'benbreeden', {1}, 1, 1, 0, 0)
+    {2}'Brittany', {1}, 1, 1, 0, 0)
+    {2}'d.ki21', {1}, 1, 1, 0, 0)
+    {2}'L.FL21', {1}, 1, 1, 0, 0)
+    {2}'mac', {1}, 1, 1, 0, 0)
+    {2}'justin', {1}, 1, 1, 0, 0)
+    {2}'aniela', {1}, 1, 1, 0, 0)
+    UPDATE USERINFO SET COMPANY = '{0}' WHERE NETWORKALIAS like '%caf2code.com'
+    Update USERINFO set ENABLE = 1 where ID != 'Guest'" -f $co, $roleId[0], $line2
+
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query $str
 Enable-D365User -Email "*caf2code*"
 update-D365User -Email "*caf2code*"
