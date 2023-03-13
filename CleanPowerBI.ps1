bcab087502b56344f89aebe7c1eaa952ee8f418f
Michael Treml <miketreml@mac.com>
2023-03-12 21:34:15 -0500

    Update CleanPowerBI.ps1

--- CleanPowerBI.ps1
+++ CleanPowerBI.ps1
@@ -1,2 +1,2 @@
 ## Clean up Power BI settings
-Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"  | Out-File -FilePath $Logfile
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE PowerBIConfig set CLIENTID = '', APPLICATIONKEY = '', REDIRECTURL = ''"
