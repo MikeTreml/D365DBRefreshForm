266cd6412f9207f19cd02b509a4cc108c0c37a54
Michael Treml <miketreml@mac.com>
2023-03-12 19:58:20 -0500

    Update BatchHold.ps1

--- BatchHold.ps1
+++ BatchHold.ps1
@@ -1,4 +1,1 @@
-$Stamp = (Get-Date).toString("yyyy-MM-dd")
-
-## Put on hold all Batch Jobs
 Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "UPDATE BatchJob SET STATUS = 0 WHERE STATUS IN (1,2,5,7)"
