f91c40c01f41fce82c0858d18a8ade288689cb94
Michael Treml <miketreml@mac.com>
2023-03-12 20:57:21 -0500

    Update SQLTracking.ps1

--- SQLTracking.ps1
+++ SQLTracking.ps1
@@ -1,2 +1,1 @@
-## Enable SQL Change Tracking
-Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "ALTER DATABASE AxDB SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)" | Out-File -FilePath $Logfile
+Invoke-DbaQuery -SqlInstance localhost -Database AxDB -Query "ALTER DATABASE AdventureWorks2012 SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)"
